2e524d6d46d13712cb6f27a1886154d57e6393fd
gilpet <petergillis11@gmail.com>
2018-05-27 16:52:34 -0300

    removed old code

--- image_gen.py
+++ image_gen.py
@@ -3,21 +3,6 @@ import random
 from PIL import Image
 from midiutil.MidiFile import MIDIFile
 
-def main():
-    pix = []
-    for i in range(10):
-        x = []
-        for q in range(10):
-            r1 = random.randint(0,255)
-            r2 = random.randint(0,255)
-            r3 = random.randint(0,255)
-            x.append((r1,r2,r3))
-        pix.append(x)
-    array = np.array(pix,dtype=np.uint8)
-    new = Image.fromarray(array)
-    new.save('new.png')
-    print list(array)
-
 def create_image():
     pixels = []
     size = 10
@@ -89,46 +74,3 @@ def get_image_data(img):
     im = Image.open(img)
     pix = list(im.getdata())
     return pix
-
-def p():
-    pix = []
-    for i in range(10):
-        x = []
-        x.append((0,50,250))
-        x.append((1,50,250))
-        x.append((2,50,250))
-        x.append((3,50,250))
-        x.append((4,50,250))
-
-        x.append((5,50,20))
-        x.append((6,50,20))
-        x.append((7,50,20))
-        x.append((8,50,20))
-        x.append((9,50,20))
-
-        pix.append(x)
-
-    array = np.array(pix,dtype=np.uint8)
-    new = Image.fromarray(array)
-    new.save('p.png')
-
-def midi():
-    mf = MIDIFile(1)
-    track = 0
-    time = 0
-    mf.addTrackName(track,time,"Sample")
-    mf.addTempo(track,time,120)
-
-    channel = 0
-    volume = 100
-
-    pitch = 60           # C4 (middle C)
-    time = 0             # start on beat 0
-    duration = 1         # 1 beat long
-    mf.addNote(track, channel, pitch, time, duration, volume)
-
-    # write it to disk
-    with open("output.mid", 'wb') as outf:
-        mf.writeFile(outf)
-
-if __name__ == "__main__": main()
